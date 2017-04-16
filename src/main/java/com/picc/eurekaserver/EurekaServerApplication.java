94ccea355012b28607138703717665e71e72c4bd
wentian <wentian1@126.com>
2017-04-16 11:14:25 +0800

    初次提交，wentian

---
+++ src/main/java/com/picc/eurekaserver/EurekaServerApplication.java
@@ -0,0 +1,16 @@
+package com.picc.eurekaserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+
+/**
+ * Created by huang on 2016/11/26.
+ */
+@EnableEurekaServer
+@SpringBootApplication
+public class EurekaServerApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(EurekaServerApplication.class, args);
+    }
+}
